ab38b4cefd69a38c7f969a1b2d8f252cbb12b331
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-05-13 10:23:29 +0500

    I edit again js file

--- menu.js
+++ menu.js
@@ -1,17 +1,16 @@
 
-
-function showSidebar(){
-    const sidebar=document.querySelector(".sidebar");
-    sidebar.style.display="flex";
-}
-function hideSidebar(){
-    const sidebar=document.querySelector(".sidebar");
-    sidebar.style.display="none";
-}
+// function showSidebar(){
+//     const sidebar=document.querySelector(".sidebar");
+//     sidebar.style.display="flex";
+// }
+// function hideSidebar(){
+//     const sidebar=document.querySelector(".sidebar");
+//     sidebar.style.display="none";
+// }
 
 let products ={
-    data :[
-
+    data: [
+            
         {
             productName:"Chicken Breast",
             category:"Chicken",
@@ -24,7 +23,7 @@ let products ={
             category:"Salad",
             price:"50",
             cart:"Add to Cart",
-
+            
             image:"./images/food4.webp",   
         }
         ,{
@@ -32,15 +31,15 @@ let products ={
             category:"Biryani",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/blg6.webp",
         }
         ,{
-            productName:"Chicken Curry",
+            productName:"Rubby Chicken Curry",
             category:"Chicken",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/food6.webp",
         }
         ,{
@@ -48,7 +47,7 @@ let products ={
             category:"Chicken",
             price:"80",
             cart:"Add to Cart",
-
+            
             image:"./images/food2.webp",
         }
         ,{
@@ -56,7 +55,7 @@ let products ={
             category:"Others",
             price:"20",
             cart:"Add to Cart",
-
+            
             image:"./images/blg4.jpg",
         }
         ,{
@@ -64,7 +63,7 @@ let products ={
             category:"Others",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/food3.webp",
         },
         {
@@ -72,15 +71,15 @@ let products ={
             category:"Salad",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/food5.webp",
         }
         ,{
-            productName:"Japanese Vegetable",
+            productName:"Japanese Vegetable Skewers ",
             category:"Others",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/blg5.webp",
         },
         {
@@ -88,7 +87,7 @@ let products ={
             category:"Others",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/blg3.webp",
         },
         {
@@ -96,7 +95,7 @@ let products ={
             category:"Others",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/blg1.webp",
         }
         ,{
@@ -104,109 +103,145 @@ let products ={
             category:"Others",
             price:"40",
             cart:"Add to Cart",
-
+            
             image:"./images/blg2.webp",
         },
     ],
-};
-
-for (let i of products.data){
-    // Create Card
-    let card=document.createElement("div");
-    //Card should have category and should stay hidden initially
-    card.classList.add("card",i.category,"hide");
-    //Image div
-    let imgContainer=document.createElement("div")
-    imgContainer.classList.add("image-container")
-    // image tag 
-    let image=document.createElement("img");
-    image.setAttribute("src",i.image);
-    imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
-    // Container
-    let container=document.createElement("div");
-    container.classList.add('container1');
-    //Product Name
-    let name=document.createElement("h5");
-    name.classList.add("product-name");
-    name.innerText=i.productName.toUpperCase() ;
-    container.appendChild(name);
-    container.style.color="#efefef"
-// price
-let price=document.createElement('h6');
-price.innerText="$" + i.price;
-container.appendChild( price);
-
-let cart=document.createElement("button");
-cart.innerText="Cart" + i.category;
-card.appendChild(container);
-/// own 
-// cart.appendChild(container);
-// console.log(cart)
-
-// console.log(container)
-// console.log(card)
-// end
- document.getElementById("products").appendChild(card)
-
-}
-//Parameter passed from  buttton (Parameter same as a category)
-function filterProduct(value){
-    //button class code
-    let butttons=document.querySelectorAll(".button-value");
-    butttons.forEach((button)=>{
-        //check if value equals innerText
-        if(value.toUpperCase()== button.innerText.toUpperCase()){
-            button.classList.add("active")
-        }else{
-            button.classList.remove("active")
-        }
-    });
-    //Select all cards
-    let elements=document.querySelectorAll('.card')
-    // loop through all cards
-    elements.forEach((element)=>{
-        if(value== "all"){
-            element.classList.remove("hide");
-        }else{
-            //
-            if(element.classList.contains(value)){
-                //
-                element.classList.remove("hide");
+    };
+    
+    for (let i of products.data){
+        // Create Card
+        let card=document.createElement("div");
+        //Card should have category and should stay hidden initially
+        card.classList.add("card",i.category,"hide");
+        //Image div
+        let imgContainer=document.createElement("div")
+        imgContainer.classList.add("image-container")
+        // image tag 
+        let image=document.createElement("img");
+        image.setAttribute("src",i.image);
+        imgContainer.appendChild(image);
+        card.appendChild(imgContainer);
+        // Container
+        let container=document.createElement("div");
+        container.classList.add('container');
+        //Product Name
+        let name=document.createElement("h5");
+        name.classList.add("product-name");
+        name.innerText=i.productName.toUpperCase() ;
+        container.appendChild(name);
+        // container.style.color="#efefef"
+        // console.log(container)
+    // price
+    let price=document.createElement('h6');
+    price.innerText="$" + i.price;
+    // price.style.color="#efefef"
+    container.appendChild(price);
+    card.appendChild(container);
+    // ============finish this block ====== //
+    // ========(SATRT) I did my work given below ======//
+    // let cart=document.createElement("button");
+    // cart.innerText="Cart" + i.category;
+    
+    // cart.appendChild(container);
+    // console.log(cart)
+    
+    // console.log(card)
+    // console.log(card)
+    // =============end i did mY work above=========///
+    
+     document.getElementById("products").appendChild(card);
+    
+    }
+    //Parameter passed from  buttton (Parameter same as a category)
+    function filterProduct(value){
+        //button class code
+        let butttons=document.querySelectorAll(".button-value");
+        butttons.forEach((button)=>{
+            //check if value equals innerText
+            if(value.toUpperCase()== button.innerText.toUpperCase()){
+                button.classList.add("active");
+            }else{
+                button.classList.remove("active")
             }
-            else{
-                //
-                element.classList.add('hide')
-            }
-        }
-    })
-}
-
-document.getElementById('search').addEventListener("click", () => {
-    // initialization
-    let searchInput = document.getElementById("search-input").value.toUpperCase(); 
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
-
-    // Check if the search input is "ALL"
-    if (searchInput === "ALL") {
-        // Show all products if the search term is "all"
-        cards.forEach((card) => {
-            card.classList.remove("hide");
         });
-    } else {
-        elements.forEach((element, index) => {
-            if (element.innerText.includes(searchInput)) {
-                cards[index].classList.remove("hide");
-            } else {
-                cards[index].classList.add("hide");
+        //Select all cards
+        let elements=document.querySelectorAll(".card");
+        // loop through all cards
+        elements.forEach((element)=>{
+            //display all cards on "all" button click
+            
+            if(value == "all"){
+                element.classList.remove("hide");
+            }else{
+                //check if element contains category class
+                if(element.classList.contains(value)){
+                    //display element based on cotegory
+                    element.classList.remove("hide");
+                }
+                else{
+                    //hide other elements
+                    element.classList.add('hide')
+                }
             }
         });
     }
-});
-
-
-//Initially display All Products
-window.onload=()=>{
-    filterProduct("all")
-}
+    
+    
+    // Search button click
+    document.getElementById('search').addEventListener("click", () => {
+        //  console.log("line190") ==mine
+        // initializations
+        let searchInput = document.getElementById("search-input").value; 
+        let elements = document.querySelectorAll(".product-name");
+        let cards = document.querySelectorAll(".card");
+       // Loop through all elements
+    
+      elements.forEach((elements,index)=>{
+        //check if text includes the search value
+        if(element.innerText.includes(searchInput.toUpperCase())){
+            //display matching card
+            cards[index].classList.remove("hide");
+        }else{
+            //hide others
+            cards[index].classList.add('hide');
+        }
+    
+      })
+    
+    });
+        
+    
+    
+    
+    
+        // Mine code below ===============
+    //     if (searchInput === "ALL") {
+    //      console.log("line198");
+    //         // Show all products if the search term is "all"
+    //         cards.forEach((card) => {
+    //             card.classList.remove("hide");
+        
+    
+    //         });
+    //     } else {
+    //         elements.forEach((element, index) => {
+    //             console.log({elementName:element.innerText.toLowerCase(),elementSearchName:searchInput})
+    //             if (element.innerText.toLowerCase().includes(searchInput)) {
+    //                 console.log('line209')
+    
+    //                 cards[index].classList.remove("hide");
+    //             } else {
+    //                 cards[index].classList.add("hide");
+    //             }
+    //         });
+    //     }
+    // });
+    
+    
+    //Initially display All Products
+    window.onload=()=>{
+        // console.log("line")
+        filterProduct("all");
+    // hideSidebar()
+    };
